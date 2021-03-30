13024e10ee264555b2160b194bcbe370c021da47
lcisnerosfiore <70279554+lcisnerosfiore@users.noreply.github.com>
2021-03-30 20:26:12 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+// Variables
+const carrito = document.querySelector('#carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
+let articulosCarrito = [];
+
+
+
+// Listeners
+cargarEventListeners();
+
+function cargarEventListeners() {
+     // Dispara cuando se presiona "Agregar Carrito"
+     listaCursos.addEventListener('click', agregarCurso);
+
+}
+
+
+// Función que añade el curso al carrito
+function agregarCurso(e) {
+     e.preventDefault();
+     // Delegation para agregar-carrito
+     if(e.target.classList.contains('agregar-carrito')) {
+          const curso = e.target.parentElement.parentElement;
+          // Enviamos el curso seleccionado para tomar sus datos
+          leerDatosCurso(curso);
+     }
+}
+
+
+function leerDatosCurso() {
+
+}
+
+function eliminarCurso() {
+
+}
+
+function vaciarCarrito() {
+
+}
